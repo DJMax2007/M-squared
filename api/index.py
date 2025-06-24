f129785c8559e31d916de52cc574f415fe508a58
DJMax2007 <anekemakua@gmail.com>
2025-06-24 13:27:07 -0700

    yh

--- api/index.py
+++ api/index.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
 import os
 import smtplib
-import cloudinary
-import cloudinary.uploader
 from email.message import EmailMessage
 
 from dotenv import load_dotenv
@@ -20,11 +18,6 @@ EMAIL_USER = os.getenv("EMAIL_USER")
 EMAIL_USER2 = os.getenv("EMAIL_USER2")
 EMAIL_PASS = os.getenv("EMAIL_PASS")
 
-# cloud details for files
-CLOUD_NAME = os.getenv("CLOUD_NAME")
-CLOUD_KEY = os.getenv("CLOUD_KEY")
-CLOUD_SECRET = os.getenv("CLOUD_SECRET")
-
 app = Flask(__name__, 
             template_folder=os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'templates'),  
             static_folder=os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'static'))
@@ -32,16 +25,7 @@ app = Flask(__name__,
 app.config["MONGO_URI"] = f"mongodb+srv://{username}:{password}@{cluster}/{database}?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
-print(EMAIL_PASS, EMAIL_USER, EMAIL_USER2)
-
-# Global Cloudinary configuration
-cloudinary.config(
-    cloud_name=CLOUD_NAME,
-    api_key=CLOUD_KEY,
-    api_secret=CLOUD_SECRET
-)
-
-def send_email_notif(name, email, phone, issue, description, file_url=None):
+def send_email_notif(name, email, phone, issue, description):
     msg = EmailMessage()
     msg["Subject"] = f"WEB APP - New Issue: {issue}"
     msg["From"] = EMAIL_USER
@@ -54,8 +38,7 @@ Email: {email}
 Phone: {phone}
 Issue Type: {issue}
 Description:
-{description}
-File Link: {file_url if file_url else 'No file uploaded'}         
+{description}         
 
 """)
     with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
@@ -63,32 +46,6 @@ File Link: {file_url if file_url else 'No file uploaded'}
         smtp.login(EMAIL_USER, EMAIL_PASS)
         smtp.send_message(msg)
     
-def handle_file_upload(file):
-    if not file or not file.filename:
-        return None
-
-    filename = file.filename
-    ext = os.path.splitext(filename)[1].lower()
-
-    allowed_exts = ['.jpg', '.jpeg', '.png', '.gif', '.pdf']
-    if ext not in allowed_exts:
-        raise ValueError("Unsupported file type")
-
-    file.seek(0, os.SEEK_END)
-    size_mb = file.tell() / (1024 * 1024)
-    file.seek(0)
-    if size_mb > 10:
-        raise ValueError(f"{filename} file too large (max 10MB)")
-
-    resource_type = "raw" if ext == '.pdf' else "image"
-
-    upload_result = cloudinary.uploader.upload(
-        file,
-        resource_type=resource_type,
-        type="upload",
-        filename=filename
-    )
-    return upload_result.get("secure_url") or upload_result.get("url")
 
 @app.route('/')
 def home():
@@ -102,40 +59,30 @@ def submit():
         phone = request.form.get('phone', '').strip()
         issue = request.form.get('issue')
         description = request.form.get('description')
-        file = request.files.get('upload')
 
         # adding the "+" sign for storage purpose
         if phone != '':
             phone = '+' + phone
 
         # TODO: deal with file uploads
-        file_url = None
+        
         try:
-            file_url = handle_file_upload(file)
-        except Exception as e:
-            return render_template("errorpage.html", error_message=str(e)), 400
-
-    
-    # Email notification
-        try:
-            # save to database
+        # save to databse
             mongo.db.users.insert_one({
                 'name': name,
                 'email': email,
                 'phone': phone,
                 'issue': issue,
-                'description':description,
-                'file': file_url
+                'description':description
             })
             # Email notification
-            send_email_notif(name, email, phone, issue, description, file_url)
+            send_email_notif(name, email, phone, issue, description)
         except Exception as e:
             print(f"Error saving to database or sending email: {e}")
             return "Something went wrong", 500
 
+
     print(f"Received name: {name}")
-    print(cloudinary.config().cloud_name)
-    print(file_url)
     return render_template("success.html", name=name)
 
 if __name__ == '__main__':
